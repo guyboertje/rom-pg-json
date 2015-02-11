a4ad6dcde8ea1d664c33f2cc3d4811bbbb058b28
Guy Boertje <guyboertje@gmail.com>
2015-02-11 13:31:48 +0000

    initial commit

---
+++ lib/rom/pg/json.rb
@@ -0,0 +1,9 @@
+require "rom/pg/json/version"
+
+module Rom
+  module Pg
+    module Json
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rom/pg/json/version.rb
@@ -0,0 +1,7 @@
+module Rom
+  module Pg
+    module Json
+      VERSION = "0.0.1"
+    end
+  end
+end
